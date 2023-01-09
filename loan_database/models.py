e0ee6e4e177b3d421fc894c41960d4a1ecd93bfe
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-09 14:34:16 -0300

    commit inicial

---
+++ loan_database/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from datetime import datetime
+
+
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    cpf = models.CharField(max_length=14)
+    telefone = models.CharField(max_length=16)
+    valor = models.FloatField(name='valor')
+    juros = models.FloatField(verbose_name='juros %',
+                              help_text='Insira a porcentagem de juros')
+    pagamento_mensal = models.FloatField(verbose_name='Valor de pagamento mensal',
+                                         help_text='Insira o valor dos juros mensais')
+    data = models.DateField(max_length=10, default=datetime.now,
+                            verbose_name='Data do empréstimo')
+    vencimento_mensal = models.DateField(
+        max_length=10, default=datetime.now, verbose_name='Data de pagamento')
+    divida_total_paga = models.BooleanField(default=False)
---
+++ loan_database/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect, HttpResponse
+from .models import Cliente
+from .forms import ClienteForm
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def clientes(request):
+    dados_cliente = {
+        'dados': Cliente.objects.all()
+    }
+    return render(request, 'loans/clientes.html', dados_cliente)
+
+@login_required
+def detalhe(request, id_cliente):
+    dados = {
+        'dados': Cliente.objects.get(pk=id_cliente)
+    }
+    return render(request, 'loans/detalhe.html', dados)
+
+@login_required
+def criar(request):
+    if request.method == 'POST':
+        cliente_form = ClienteForm(request.POST)
+        if cliente_form.is_valid():
+            cliente_form.save()
+        return redirect('clientes')
+    else:
+        cliente_form = ClienteForm()
+        formulario = {
+            'formulario': cliente_form
+        }
+        return render(request, 'loans/novo_emprestimo.html', context=formulario)
+
+@login_required
+def editar(request, id_cliente):
+    cliente = Cliente.objects.get(pk=id_cliente)
+    if request.method == 'GET':
+        formulario = ClienteForm(instance=cliente)
+        return render(request, 'loans/novo_emprestimo.html', {'formulario': formulario})
+    else:
+        formulario = ClienteForm(request.POST, instance=cliente)
+        if formulario.is_valid():
+            formulario.save()
+        return redirect('clientes')
+
+@login_required
+def excluir(request, id_cliente):
+    cliente = Cliente.objects.get(pk=id_cliente)
+    if request.method == 'POST':
+        cliente.delete()
+        return redirect('clientes')
+    return render(request, 'loans/confirmar_exclusao.html', {'item': cliente})
