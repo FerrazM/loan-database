5ada1ccf88d9dd2829277fa5344a4c58bb70b530
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-11 11:20:52 -0300

    commit models.py2

--- loan_database/forms.py
+++ loan_database/forms.py
@@ -1,11 +1,8 @@
 from django.forms import ModelForm
 from .models import Cliente
 
+
 class ClienteForm(ModelForm):
     class Meta:
         model = Cliente
         fields = '__all__'
-        
-    def __init__(self,*args,**kwargs):
-        super().__init__(*args,**kwargs)
-        self.fields['cpf'].widget.attrs.update({'class': 'cpf'})
--- loan_database/models.py
+++ loan_database/models.py
@@ -6,10 +6,10 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=50)
     cpf = models.CharField(max_length=14)
     telefone = models.CharField(max_length=16)
-    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    valor = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     juros = models.FloatField(verbose_name='juros %',
                               help_text='Insira a porcentagem de juros')
-    pagamento_mensal = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Valor de pagamento mensal',
+    pagamento_mensal = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Valor de pagamento mensal',
                                            help_text='Insira o valor dos juros mensais')
     data = models.DateField(max_length=10,
                             verbose_name='Data do empréstimo')
