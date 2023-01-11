181b0444e29395b12aaacdca3659f40423de2e51
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-10 22:57:28 -0300

    commit mask 2

--- loan_database/models.py
+++ loan_database/models.py
@@ -11,9 +11,9 @@ class Cliente(models.Model):
                               help_text='Insira a porcentagem de juros')
     pagamento_mensal = models.FloatField(verbose_name='Valor de pagamento mensal',
                                          help_text='Insira o valor dos juros mensais')
-    data = models.DateField(max_length=10, default=datetime.now,
+    data = models.DateField(max_length=10,
                             verbose_name='Data do empréstimo')
     vencimento_mensal = models.DateField(
-        max_length=10, default=datetime.now)
+        max_length=10)
     divida_total_paga = models.BooleanField(default=False,
                                             verbose_name='Dívida paga')
