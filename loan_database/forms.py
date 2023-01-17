2e3d9040242914f1ed9338423003e244c1f99f59
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-17 12:14:30 -0300

    mask- cpf - ok

--- loan_database/forms.py
+++ loan_database/forms.py
@@ -7,11 +7,6 @@ class ClienteForm(forms.ModelForm):
         model = Cliente
         fields = '__all__'
 
-
-def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.fields['cpf'].widget.attrs.update({'class': 'mask-cpf'})
-    self.fields['telefone'].widget.attrs.update({'class': 'mask-telefone'})
-    self.fields['vencimento_data'].widget.attrs.update({'class': 'mask-data'})
-    self.fields['vencimento_mensal'].widget.attrs.update(
-        {'class': '.mask-vencimento'})
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['cpf'].widget.attrs.update({'class': 'mask-cpf'})
