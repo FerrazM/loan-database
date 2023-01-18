d1b9d3c1c67bea298af6c53b2e1a8330d2755aa1
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-18 14:24:51 -0300

    v2.6

--- loan_database/models.py
+++ loan_database/models.py
@@ -12,8 +12,8 @@ class Cliente(models.Model):
     pagamento_mensal = models.CharField(max_length=22, verbose_name='Valor de pagamento mensal',
                                         help_text='Insira o valor dos juros mensais')
     data = models.DateField(max_length=10,
-                            verbose_name='Data do empréstimo')
+                            verbose_name='Data do empréstimo', null=True)
     vencimento_mensal = models.DateField(
-        max_length=10)
+        max_length=10, null=True)
     divida_total_paga = models.BooleanField(default=False,
                                             verbose_name='Dívida paga')
