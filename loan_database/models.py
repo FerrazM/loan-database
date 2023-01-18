18787c564e4e28c6cf8fff899d3c9be6fa18389a
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-18 13:40:53 -0300

    migrations

--- loan_database/models.py
+++ loan_database/models.py
@@ -1,20 +1,19 @@
 from django.db import models
-from datetime import datetime
 
 
 class Cliente(models.Model):
     nome = models.CharField(max_length=50)
     cpf = models.CharField(max_length=14)
     telefone = models.CharField(
-        max_length=16, help_text='Insira o telefone com ddd')
+        max_length=16, help_text='Insira o telefone com DDD')
     valor = models.CharField(max_length=22, verbose_name='Valor R$')
     juros = models.CharField(max_length=6, verbose_name='juros %',
                              help_text='Insira a porcentagem de juros')
     pagamento_mensal = models.CharField(max_length=22, verbose_name='Valor de pagamento mensal',
                                         help_text='Insira o valor dos juros mensais')
-    data = models.DateField(max_length=10, default=datetime.now().strftime('%d,%m,%Y'),
+    data = models.DateField(max_length=10,
                             verbose_name='Data do empréstimo')
-    vencimento_mensal = models.DateField(default=datetime.now().strftime('%d,%m,%Y'),
-                                         max_length=10)
+    vencimento_mensal = models.DateField(
+        max_length=10)
     divida_total_paga = models.BooleanField(default=False,
                                             verbose_name='Dívida paga')
