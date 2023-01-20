d769491e8f3039eec88a6948f6cae07e75c59927
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-20 15:52:54 -0300

    update setting database

--- projeto_loan_database/settings.py
+++ projeto_loan_database/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import whitenoise
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -90,7 +90,8 @@ DATABASES = {
         'PORT': '7185',
     }
 }
-
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
