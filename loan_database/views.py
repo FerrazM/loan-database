1a8fc465fbdfa83782d2bfefa24c4f652f8040a4
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-23 17:46:11 -0300

    datefield mudanças

--- loan_database/views.py
+++ loan_database/views.py
@@ -3,7 +3,7 @@ from .models import Cliente
 from .forms import ClienteForm
 from django.contrib.auth.decorators import login_required
 from django.db.models import Sum
-from datetime import datetime, timedelta
+
 
 
 @login_required
@@ -75,9 +75,9 @@ def somaemprestimos(request):
 def balancomensal(request):
     inicio_mes = request.POST.get('start_date')
     fim_mes = request.POST.get('end_date')
-    soma_valor = Cliente.objects.filter(usuario=request.user, data__range=[
+    soma_valor = Cliente.objects.filter(usuario=request.user, valor__range=[
                                         inicio_mes, fim_mes]).aggregate(Sum('valor'))
-    soma_pagamento = Cliente.objects.filter(usuario=request.user, vencimento_mensal__range=[
+    soma_pagamento = Cliente.objects.filter(usuario=request.user, juros_mes__range=[
                                             inicio_mes, fim_mes]).aggregate(Sum('juros_mes'))
     context = {'soma_valor': soma_valor, 'soma_pagamento': soma_pagamento}
     return render(request, 'loans/balanco_mensal.html', context)
