d0d53e76de6216d90923a7ce478147dea88f73e5
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-01-26 19:33:17 -0300

    tentativa checkbox

--- staticfiles/js/checkbox.js
+++ staticfiles/js/checkbox.js
@@ -1,6 +1,6 @@
-function handleChange(checkbox1, clientId) {
-    var paymentField = document.querySelector(`#payment-field-${clientId}`);
-    if (checkbox1.checked) {
+function handleChange(checkbox) {
+    var paymentField = document.querySelector("#paymentField-" + checkbox.name.split("-")[1])
+    if (checkbox.checked) {
         paymentField.classList.remove("red-background");
         paymentField.classList.add("green-background");
     } else {
