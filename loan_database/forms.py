2037cbf8fee0f39ba73f6538cda2bf30c5f8f0fb
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-02-01 13:07:55 -0300

    correção Cliente form

--- loan_database/forms.py
+++ loan_database/forms.py
@@ -19,9 +19,6 @@ class ClienteForm(forms.ModelForm):
         self.fields['vencimento_mensal'].widget.attrs.update(
             {'class': 'mask-vencimento'})
         self.fields['usuario'].widget = forms.HiddenInput()
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         if self.instance and self.instance.checkbox1:
             self.fields['juros_mes'].widget.attrs['class'] = 'green-field'
         else:
