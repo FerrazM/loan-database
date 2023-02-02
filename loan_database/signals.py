5f4462b9158347fbaf5276ece6a8c3347dbf4f64
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-02-02 15:56:08 -0300

    altreção checkbox

---
+++ loan_database/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
+
+from .models import Cliente
+
+
+@receiver(post_save, sender=Cliente)
+def check_uncheck_checkbox1(sender, instance, **kwargs):
+    if instance.vencimento_mensal == timezone.now().date():
+        instance.mensalidade_paga = False
+        instance.save()
