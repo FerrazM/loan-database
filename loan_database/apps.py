e82d219fe481f5c173779f50b35c64eb3965ffbb
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-02-02 16:16:28 -0300

    correção signals

--- loan_database/apps.py
+++ loan_database/apps.py
@@ -5,9 +5,5 @@ class LoanDatabaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'loan_database'
 
-
-class LoanDatabaseConfig(AppConfig):
-    name = 'loan_database'
-
     def ready(self):
         import loan_database.signals
