8c3b2a62c4988291e1183419a473045e130e8f7e
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-02-02 16:08:58 -0300

    introdução signals

--- loan_database/apps.py
+++ loan_database/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class LoanDatabaseConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'loan_database'
+
+
+class LoanDatabaseConfig(AppConfig):
+    name = 'loan_database'
+
+    def ready(self):
+        import loan_database.signals
--- loan_database/signals.py
+++ loan_database/signals.py
@@ -1,12 +1,11 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from django.utils import timezone
+from loan_database.models import Cliente
+import datetime
 
-from .models import Cliente
 
-
-@receiver(post_save, sender=Cliente)
+@receiver(pre_save, sender=Cliente)
 def check_uncheck_checkbox1(sender, instance, **kwargs):
-    if instance.vencimento_mensal == timezone.now().date():
+    today = datetime.date.today()
+    if instance.vencimento_mensal < today:
         instance.mensalidade_paga = False
-        instance.save()
