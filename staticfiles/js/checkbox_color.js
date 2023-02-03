783085253526a3124a488410cec947a437e292b3
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-02-03 16:03:03 -0300

    add css checkbox

--- staticfiles/js/checkbox_color.js
+++ staticfiles/js/checkbox_color.js
@@ -1,10 +1,13 @@
-const checkbox = document.getElementById("checkbox1");
-const jurosMes = document.getElementById("juros_mes");
+console.log(jurosMes);
+const checkbox1 = document.querySelector("#checkbox1");
+const jurosMes = document.querySelector("#juros_mes");
 
-checkbox.addEventListener("change", function () {
-    if (this.checked) {
+checkbox1.addEventListener("change", function () {
+    if (checkbox1.checked) {
+        jurosMes.classList.remove("red");
         jurosMes.classList.add("green");
     } else {
         jurosMes.classList.remove("green");
+        jurosMes.classList.add("red");
     }
 });
