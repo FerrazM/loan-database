cffceda0c255066d9b643bfae59613445fa6b777
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-02-10 20:03:45 -0300

    tentativa 5

--- static/js/juros_mes.js
+++ static/js/juros_mes.js
@@ -7,9 +7,10 @@ function updatePayment() {
     let valor = parseFloat(document.getElementById("id_valor").value);
     let juros = document.getElementById("id_juros").value;
 
+
     if (valor && juros) {
         let jurosDecimal = parseFloat(juros.replace("%", "")) / 100;
         let juros_mes = valor * jurosDecimal;
-        document.getElementById("id_juros_mes").value = juros_mes.toFixed(2);
+        document.getElementById("id_juros_mes").value = juros_mes.toFixed(2).replace(".", ",") + "%";
     }
 }
