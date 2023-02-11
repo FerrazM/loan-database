467eef14b36cfa3e532663bf15f7658c3e385568
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-02-11 15:06:46 -0300

    att js

--- staticfiles/js/juros_mes.js
+++ staticfiles/js/juros_mes.js
@@ -1,13 +1,15 @@
-document.getElementById("id_valor").addEventListener("input", updatePayment);
-document.getElementById("id_juros").addEventListener("input", updatePayment);
+window.addEventListener("load", function () {
+    document.getElementById("id_valor").addEventListener("input", updatePayment);
+    document.getElementById("id_juros").addEventListener("input", updatePayment);
+});
 
 function updatePayment() {
-  let valor = document.getElementById("id_valor").value;
-  let juros = document.getElementById("id_juros").value;
-  
-  if (valor && juros) {
-    let jurosDecimal = parseFloat(juros.replace("%", "")) / 100;
-    let pagamentoMensal = valor * jurosDecimal;
-    document.getElementById("id_pagamento_mensal").value = pagamentoMensal.toFixed(2);
-  }
+    let valor = parseFloat(document.getElementById("id_valor").value.replace(",", "."));
+    let juros = document.getElementById("id_juros").value;
+
+    if (valor && juros) {
+        let jurosDecimal = parseFloat(juros.replace("%", "")) / 100;
+        let juros_mes = valor * jurosDecimal;
+        document.getElementById("id_juros_mes").value = juros_mes.toFixed(2);
+    }
 }
