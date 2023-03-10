1ae996308e3e0015f1ff43feaac8c6a624a7cd0d
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-03-10 15:10:15 -0300

    att parcelas pagas

--- static/js/parcelas_pagas.js
+++ static/js/parcelas_pagas.js
@@ -1,5 +1,5 @@
 // selecione a checkbox1 e adicione um ouvinte de evento de clique
-const checkbox1 = document.getElementById('id_mensalidade_paga');
+const checkbox1 = document.getElementById('id_checkbox1');
 checkbox1.addEventListener('click', function () {
     // se a checkbox1 estiver marcada, atualize a quantidade de parcelas_pagas
     if (checkbox1.checked) {
