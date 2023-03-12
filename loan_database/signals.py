92c1263ce13d817ec5b6bd6ecf6036bb1f307dea
Matheus Ferraz <matheusferrazo_@hotmail.com>
2023-03-12 12:32:09 -0300

    att signals

--- loan_database/signals.py
+++ loan_database/signals.py
@@ -1,11 +1,12 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
-from loan_database.models import Cliente
-import datetime
+from django.utils import timezone
+from .models import Cliente
 
 
-@receiver(pre_save, sender=Cliente)
+@receiver(post_save, sender=Cliente)
 def check_uncheck_checkbox1(sender, instance, **kwargs):
-    today = datetime.date.today()
+    today = timezone.now().date()
     if instance.vencimento_mensal < today:
         instance.checkbox1 = False
+        instance.save(update_fields=['checkbox1'])
